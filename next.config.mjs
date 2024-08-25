43b97cf99dd48b160b8a6f04359ff9e5da814212
Baran Arslan <69631182+thebarslan@users.noreply.github.com>
2024-08-25 17:57:09 +0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
    images: {
-      domains: ["i.imgur.com"], // Add 'imgur.com' here
+      domains: ["i.imgur.com", "api.telegram.org"], // Add 'imgur.com' here
    },
 };
 
